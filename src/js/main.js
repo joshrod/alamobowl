a2e5477717c982bdcb7661a39552af408b8903ec
Joshua Rodriguez <j_rodriguez@m110.local>
2019-07-03 13:58:37 -0500

    Sidebar implemented. Header moved outside of main to accommodate.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
-console.log('Script should start working.');
+const menu = document.querySelector('.menu');
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('main');
+const footer = document.querySelector('footer');
+const navbar = document.querySelector('header');
+
+window.onload = () => {
+    
+
+    menu.onclick = () => {
+        if (!sidebar.classList.contains('sidebar-open')) {
+            sidebar.classList.add('sidebar-open');
+            mainContent.classList.add('open-sidebar');
+            navbar.classList.add('open-sidebar');
+            footer.classList.add('open-sidebar');
+
+        }
+        else {
+            sidebar.classList.remove('sidebar-open');
+            mainContent.classList.remove('open-sidebar');
+            navbar.classList.remove('open-sidebar');
+            footer.classList.remove('open-sidebar');
+        }
+    }
+}
